4105bf9225d6fede0ace8f2cf26f6e4a23e32614
julibert <julianbermudez@eprosima.com>
2020-04-23 11:25:34 +0000

    Add app_info.sh to apps.

---
+++ agent/app_info.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+export APP_PACKAGE_NAME=micro_ros_agent
+export APP_BINARY_NAME=micro_ros_agent
+export APP_OUTPUT_NAME=micro_ros_agent
---
+++ weather_publisher/app_info.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+export APP_PACKAGE_NAME=weather_publisher
+export APP_BINARY_NAME=weather_publisher
+export APP_OUTPUT_NAME=weather_publisher
