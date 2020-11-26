4f2a59b7e9f325a68a4abc961b63bc64c80e72fc
Jose Antonio Moral <joseantoniomoralparras@gmail.com>
2020-11-26 10:30:54 +0100

    Fix compilation for agent_lite demo

--- agent_lite/app_info.sh
+++ agent_lite/app_info.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-export APP_PACKAGE_NAME=micro_ros_agent
-export APP_BINARY_NAME=micro_ros_agent
+export APP_PACKAGE_NAME=microxrcedds_agent
+export APP_BINARY_NAME=MicroXRCEAgent
 export APP_OUTPUT_NAME=micro_ros_agent_lite
