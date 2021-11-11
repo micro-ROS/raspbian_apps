50599695c98c8681db96f41f5c314c1d754c859c
Antonio Cuadros <49162117+Acuadros95@users.noreply.github.com>
2021-11-11 08:24:21 +0100

    Update with galactic (#17)

--- weather_agent/app/main.cpp
+++ weather_agent/app/main.cpp
@@ -1,17 +1,35 @@
 #include <uxr/agent/transport/udp/UDPv4AgentLinux.hpp>
 #include <uxr/agent/transport/serial/TermiosAgentLinux.hpp>
 #include <uxr/agent/transport/serial/baud_rate_table_linux.h>
+#include <uxr/agent/transport/endpoint/SerialEndPoint.hpp>
 
 #include <termios.h>
 #include <fcntl.h>
+#include <fstream>
 
 int main(int argc, char** argv)
 {
-    uint16_t port = std::stoi(argv[1]);
-    char* device = argv[2];
+    // TODO: use args for UDP port?
+    uint16_t port = 8888;
+    std::string port_name;
 
-    struct termios attr;
+    // Read serial port from file
+    std::ifstream port_file("/tmp/uros/port.log");
 
+    if (port_file.is_open())
+    {
+        getline (port_file, port_name);
+        port_file.close();
+    }
+    else
+    {
+        std::cout << "Bridge script not initialized" << std::endl;
+        return -1;
+    }
+
+    struct termios attr = {};
+
+    /* Setting CONTROL OPTIONS. */
     attr.c_cflag |= unsigned(CREAD);    // Enable read.
     attr.c_cflag |= unsigned(CLOCAL);   // Set local mode.
     attr.c_cflag &= unsigned(~PARENB);  // Disable parity.
@@ -40,7 +58,7 @@ int main(int argc, char** argv)
     attr.c_oflag &= unsigned(~OPOST);   // Set raw output.
 
     /* Setting OUTPUT CHARACTERS. */
-    attr.c_cc[VMIN] = 10;
+    attr.c_cc[VMIN] = 1;
     attr.c_cc[VTIME] = 1;
 
     /* Setting baudrate. */
@@ -52,9 +70,97 @@ int main(int argc, char** argv)
     agent_udp4.set_verbose_level(6);
     agent_udp4.start();
 
-    eprosima::uxr::TermiosAgent agent_serial(device, O_RDWR | O_NOCTTY, attr, 0, eprosima::uxr::Middleware::Kind::CED);
+    eprosima::uxr::TermiosAgent agent_serial(port_name.c_str(), O_RDWR | O_NOCTTY, attr, 0, eprosima::uxr::Middleware::Kind::CED);
     agent_serial.set_verbose_level(6);
     agent_serial.start();
+
+    // Create crazyflie sesion
+    uint32_t key = 0x12345678;
+    uint8_t session = 0x81;
+    uint16_t mtu = 30;
+    eprosima::uxr::Agent::OpResult result;
+
+    agent_serial.create_client(key, session, mtu, eprosima::uxr::Middleware::Kind::CED, result);
+
+	if (eprosima::uxr::Agent::OpResult::OK != result)
+	{
+		std::cout << "Create session failed, error code: " << result << std::endl;
+		return -1;
+	}
+
+    const eprosima::uxr::SerialEndPoint endpoint = eprosima::uxr::SerialEndPoint(0);
+    agent_serial.establish_session(endpoint, key, session);
+
+    // Create crazyflie entities
+    uint16_t participant_key = 0x01;
+    uint16_t topic_key = 0x01;
+    uint16_t subscriber_key = 0x01;
+    uint16_t datawriter_key = 0x01;
+
+    std::string topic_name = "Float__Sequence";
+
+    std::string participant_xml = "";
+    std::string topic_xml = "rt/" + topic_name;
+    std::string subscriber_xml = "";
+    std::string datareader_xml = "rt/" + topic_name;
+
+    uint16_t domain_id = 0;
+
+    agent_serial.create_participant_by_xml(
+            key,
+            participant_key,
+            domain_id,
+            participant_xml.c_str(),
+            eprosima::uxr::Agent::REUSE_MODE,
+            result);
+
+	if (eprosima::uxr::Agent::OpResult::OK != result)
+	{
+		std::cout << "Create participant failed, error code: " << result << std::endl;
+		return -1;
+	}
+
+    agent_serial.create_topic_by_xml(
+            key,
+            topic_key,
+            participant_key,
+            topic_xml.c_str(),
+            eprosima::uxr::Agent::REUSE_MODE,
+            result);
+
+	if (eprosima::uxr::Agent::OpResult::OK != result)
+	{
+		std::cout << "Create topic failed, error code: " << result << std::endl;
+		return -1;
+	}
+
+    agent_serial.create_subscriber_by_xml(
+            key,
+            subscriber_key,
+            participant_key,
+            subscriber_xml.c_str(),
+            eprosima::uxr::Agent::REUSE_MODE,
+            result);
+
+	if (eprosima::uxr::Agent::OpResult::OK != result)
+	{
+		std::cout << "Create subscriber failed, error code: " << result << std::endl;
+		return -1;
+	}
+
+    agent_serial.create_datareader_by_xml(
+            key,
+            datawriter_key,
+            subscriber_key,
+            datareader_xml.c_str(),
+            eprosima::uxr::Agent::REUSE_MODE,
+            result);
+
+	if (eprosima::uxr::Agent::OpResult::OK != result)
+	{
+		std::cout << "Create Datareader failed, error code: " << result << std::endl;
+		return -1;
+	}
 
     while (true) { std::this_thread::sleep_for(std::chrono::seconds(1)); }
 
